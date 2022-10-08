26f3bb7492b48fccf2b47b793f0bf4cd14a60619
Lauren Smith <lsmith24@student.sl.on.ca>
2022-10-08 18:08:07 -0400

    steps 1-4 completed and this is the initial commit

---
+++ data/weather.js
@@ -0,0 +1,218 @@
+/**
+ * kingston weather Daily
+ */
+const MondayKing = {
+    POP:0.3,
+    highTemp:21,
+    lowTemp:13,
+    currentTemp:19,
+    windSpeed:29,
+    description:'A mix of sun and clouds',
+    dayOfweek:'monday'
+};
+const TuesdayKing = {
+    POP:0.7,
+    highTemp:15,
+    lowTemp:4,
+    currentTemp:15,
+    windSpeed:28,
+    description:'A few Showers',
+    dayOfweek:'tuesday'
+};
+const WednesdayKing = {
+    POP:0.1,
+    highTemp:11,
+    lowTemp:8,
+    currentTemp:11,
+    windSpeed:27,
+    description:'Sunny',
+    dayOfweek:'wednesday'
+};
+const ThursdayKing = {
+    POP:0.3,
+    highTemp:13,
+    lowTemp:9,
+    currentTemp:13,
+    windSpeed:21,
+    description:'Cloudy with sunny breaks',
+    dayOfweek:'thursday'
+};
+const FridayKing = {
+    POP:0.1,
+    highTemp:14,
+    lowTemp:10,
+    currentTemp:14,
+    windSpeed:19,
+    description:'Sunny',
+    dayOfweek:'friday'
+};
+const SaturdayKing = {
+    POP:0.1,
+    highTemp:14,
+    lowTemp:9,
+    currentTemp:14,
+    windSpeed:18,
+    description:'Sunny',
+    dayOfweek:'saturday'
+};
+const SundayKing = {
+    POP:0.6,
+    highTemp:16,
+    lowTemp:11,
+    currentTemp:16,
+    windSpeed:33,
+    description:'A few showers',
+    dayOfweek:'sunday'
+};
+
+/**
+ * Toronto Weather Daily
+ */
+ const MondayTor = {
+    POP:0.3,
+    highTemp:22,
+    lowTemp:8,
+    currentTemp:21,
+    windSpeed:26,
+    description:'Cloudy with sunny breaks',
+    dayOfweek:'monday'
+ };
+ const TuesdayTor = {
+    POP:0.4,
+    highTemp:11,
+    lowTemp:4,
+    currentTemp:11,
+    windSpeed:25,
+    description:'Chance of a shower',
+    dayOfweek:'tuesday'
+ };
+ const WednesdayTor = {
+    POP:0.2,
+    highTemp:12,
+    lowTemp:5,
+    currentTemp:12,
+    windSpeed:23,
+    description:'A mix of sun and clouds',
+    dayOfweek:'wednesday'
+ };
+ const ThursdayTor = {
+    POP:0.2,
+    highTemp:15,
+    lowTemp:6,
+    currentTemp:15,
+    windSpeed:21,
+    description:'Mainly Sunny',
+    dayOfweek:'thursday'
+ };
+ const FridayTor = {
+    POP:0.2,
+    highTemp:16,
+    lowTemp:7,
+    currentTemp:16,
+    windSpeed:16,
+    description:'Mainly Cloudy',
+    dayOfweek:'friday'
+ };
+ const SaturdayTor = {
+    POP:0.4,
+    highTemp:16,
+    lowTemp:9,
+    currentTemp:16,
+    windSpeed:21,
+    description:'A chance of showers',
+    dayOfweek:'saturday'
+ };
+ const SundayTor = {
+    POP:0.6,
+    highTemp:19,
+    lowTemp:11,
+    currentTemp:18,
+    windSpeed:31,
+    description:'A few Showers',
+    dayOfweek:'sunday'
+ };
+
+ /**
+  * Ottawa Weather Daily
+  */
+  const MondayOtt = {
+    POP:0.3,
+    highTemp:22,
+    lowTemp:11,
+    currentTemp:21,
+    windSpeed:11,
+    description:'A mix of sun and clouds',
+    dayOfweek:'monday'
+  };
+  const TuesdayOtt = {
+    POP:0.7,
+    highTemp:12,
+    lowTemp:3,
+    currentTemp:12,
+    windSpeed:21,
+    description:'Light Rain',
+    dayOfweek:'tuesday'
+  };
+  const WednesdayOtt = {
+    POP:0.2,
+    highTemp:11,
+    lowTemp:6,
+    currentTemp:11,
+    windSpeed:22,
+    description:'Mainly Sunny',
+    dayOfweek:'wednesday'
+  };
+  const ThursdayOtt = {
+    POP:0.3,
+    highTemp:13,
+    lowTemp:6,
+    currentTemp:13,
+    windSpeed:14,
+    description:'A mix of sun and clouds',
+    dayOfweek:'thursday'
+  };
+  const FridayOtt = {
+    POP:0.1,
+    highTemp:15,
+    lowTemp:6,
+    currentTemp:15,
+    windSpeed:14,
+    description:'Sunny',
+    dayOfweek:'friday'
+  };
+  const SaturdayOtt = {
+    POP:0.1,
+    highTemp:15,
+    lowTemp:7,
+    currentTemp:15,
+    windSpeed:11,
+    description:'Sunny',
+    dayOfweek:'saturday'
+  };
+  const SundayOtt = {
+    POP:0.4,
+    highTemp:17,
+    lowTemp:8,
+    currentTemp:17,
+    windSpeed:25,
+    description:'Mainly Sunny',
+    dayOfweek:'sunday'
+  };
+
+/**
+ * weekly weather 
+ */
+const kingstonWeekly = [MondayKing,TuesdayKing,WednesdayKing,ThursdayKing,FridayKing,SaturdayKing,SundayKing];
+const TorontoWeekly = [MondayTor,TuesdayTor,WednesdayTor,ThursdayTor,FridayTor,SaturdayTor,SundayTor];
+const OttawaWeekly=[MondayOtt,TuesdayOtt,WednesdayOtt,ThursdayOtt,FridayOtt,SaturdayOtt,SundayOtt];
+
+/**
+ * weather for areas
+ */
+ const weather={
+    kingston:kingstonWeekly,
+    toronto:TorontoWeekly,
+    ottawa:OttawaWeekly
+};
+
+module.exports = weather;
---
+++ server.js
@@ -0,0 +1,91 @@
+/**
+ * -----------------[PERSONAL INFO]--------------------------------
+ * NAME:Lauren Smith
+ * COURSE: COMP206
+ * MODULE: ASSIGNMENT 1 - WEATHER APP
+ * -----------------[END]------------------------------------------
+ */
+const express = require('express');
+const ejs = require('ejs');
+
+const weather = require('./data/weather');
+const app = express();
+/**
+ * -----------------[BEGINING OF MIDDLEWARE/CONFIGURATION]--------------------------------
+ */
+ app.set("view engine", "ejs");
+ app.use(express.static('public'));
+ /**
+ * -----------------[END OF MIDDLEWARE/CONFIGURATION]--------------------------------
+ */
+
+/**
+ * -----------------[BEGINING OF ROUTES]--------------------------------
+ */
+
+app.get('/weather',(request,response)=>{
+    /**
+     * Route : /weather
+     * Method: GET
+     * Params: none
+     * Query Params: none 
+     * Permission: Public
+     * Purpose: to send weather data for the three cities as a responce
+     * Responce: JSON Object
+     */
+    let result = weather; 
+    response.send(result);
+});
+
+app.get('/weather/:location',(request,response)=>{
+    /**
+     * Route : /weather/:location
+     * Method: GET
+     * Params: location
+     * Query Params: none 
+     * Permission: Public
+     * Purpose: to send weather data for the given location as a responce
+     * Responce: JSON Object
+     */
+    let result = weather; 
+    if(typeof request.params.location !== 'undefined')
+    {
+        const location = request.params.location.toLowerCase();
+        result = weather[location];
+    }
+    response.send(result);
+});
+
+
+app.get('/weather/:location/:day',(request,response)=>{
+    /**
+     * Route : /weather/:location/:day
+     * Method: GET
+     * Params: location,day
+     * Query Params: none 
+     * Permission: Public
+     * Purpose: to send weather data for the given location for a given day as a responce
+     * Responce: JSON Object
+     */
+    let result = weather; 
+    if(typeof request.params.location !== 'undefined')
+    {
+        const location = request.params.location.toLowerCase();
+        result = weather[location];
+        if(typeof request.params.day !== 'undefined')
+        {
+            const day = request.params.day.toLowerCase();
+            const weekly = weather[location];
+            const daily = weekly.find(daily => daily.dayOfweek === day);
+            console.log(daily);
+            result = daily;
+        }
+    }
+    response.send(result);
+});
+/**
+* -----------------[END OF ROUTES]--------------------------------
+*/
+app.listen(3000,()=>{
+    console.log("App is listening on http://localhost:3000");
+});
